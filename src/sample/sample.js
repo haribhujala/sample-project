7785d0f179e641872194e8dca01acc261fc9a7c7
haribhujala <hariprasadbhujala@gmail.com>
2024-03-03 15:42:04 +0530

    I have created sample folder and added some files for test.

---
+++ src/sample/sample.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 class="heading">Welcome to my page</h1>
+      <h2 class="subheading">welcome to my page</h2>
+      <p class="paragraph">
+        This is my boiler plate page, I'm going to make it better by using css.
+        I will make it dynamic by using javascript.
+      </p>
+    </div>
+  );
+}
+
+export default App;
